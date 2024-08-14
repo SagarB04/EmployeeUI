b69316c24bc42e89f7ea3362c9b0711e2102c597
Sagar Barman <barmansagar01@gmail.com>
2024-08-14 20:07:36 +0530

    fetched data from db

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,38 +1,43 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Popup from "reactjs-popup";
 
 const Employees = () => {
 
   const [list, setList] = useState([
     {
-      employeeId: 121,
-      firstName: "Lex",
-      lastName: "Luthar",
-      phone: 1223211,
-      salary: 45000,
-      departmentId: 89,
-      jobId: "SA_MAN",
-    },
-    {
-      employeeId: 131,
-      firstName: "Alex",
-      lastName: "Turner",
-      phone: 875311,
-      salary: 75000,
-      departmentId: 99,
-      jobId: "IT_MAN",
+      id: 100,
+      fname: "Sagar",
+      lname: "Barman",
+      email: "SKINGdcfvsd",
+      phone: "515.123.4567",
+      hireDate: "2003-06-17",
+      jobId: "AD_PRES",
+      salary: 24000,
+      managerId: null,
+      deptId: 90
     }
+
+
   ]);
 
+  useEffect(() => {
+    getAllEmployee();
+  }, [list])
 
+
+  const getAllEmployee = async () => {
+    let a = await fetch("http://localhost:8090/employees");
+    let data = await a.json();
+    setList(data);
+  }
 
 
   return (
-    <div>
+    <div className="">
 
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg md:m-5">
-        <table className="w-full text-sm rtl:text-right text-gray-500 dark:text-gray-400 text-center">
-          <caption className="p-5 text-lg font-semibold text-left rtl:text-right text-gray-800 bg-gray-200 ">
+      <div className="relative overflow-x-auto shadow-md">
+        <table className="w-full text-sm rtl:text-right text-gray-500 dark:text-gray-400 text-center rounded-lg overflow-hidden shadow-xl shadow-blue-950">
+          <caption className="p-5 text-lg font-bold text-left rtl:text-right text-gray-600 bg-gray-200">
             Employees List
           </caption>
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -41,10 +46,10 @@ const Employees = () => {
                 Id
               </th>
               <th scope="col" className="px-6 py-3">
-                First Name
+                Name
               </th>
               <th scope="col" className="px-6 py-3">
-                Last Name
+                Job Id
               </th>
               <th scope="col" className="px-6 py-3">
                 Phone
@@ -54,103 +59,103 @@ const Employees = () => {
               </th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="">
 
             {
               list.map((emp) => {
-                return <tr key={emp.employeeId} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                return <tr key={emp.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 ">
                   <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {emp.employeeId}
+                    {emp.id}
                   </th>
                   <td className="px-6 py-4">
-                    {emp.firstName}
+                    {emp.fname + " " + emp.lname}
                   </td>
                   <td className="px-6 py-4">
-                    {emp.lastName}
+                    {emp.jobId}
                   </td>
                   <td className="px-6 py-4">
                     {emp.phone}
                   </td>
                   <td className="px-6 py-4">
-                  <Popup
-          trigger={<button className="button font-medium text-blue-500  hover:underline"> View </button>}
-          modal
-          nested
-        >
-          {(close) => (
-            <div className=" text-slate-900 bg-white rounded-lg w-96 shadow-lg shadow-neutral-700">
-              <div className="flex rounded-t-lg justify-end text-red-600 font-extrabold text-2xl bg-blue-950">
-                <button className="px-2" onClick={close}>
-                  &times;
-                </button>
-              </div>
+                    <Popup
+                      trigger={<button className="button font-medium text-blue-500 hover:underline"> View </button>}
+                      modal
+                      nested
+                    >
+                      {(close) => (
+                        <div className=" text-slate-900 bg-white rounded-lg w-96 shadow-lg shadow-neutral-700">
+                          <div className="flex rounded-t-lg justify-end text-red-600 font-extrabold text-2xl bg-blue-950">
+                            <button className="px-2" onClick={close}>
+                              &times;
+                            </button>
+                          </div>
 
-              <div className="text-center bg-blue-950 text-slate-100 pb-4 font-semibold"> Employee detail </div>
+                          <div className="text-center bg-blue-950 text-slate-100 pb-4 font-semibold"> Employee detail </div>
 
-              <div className="p-4 bg-gray-200 w-full">
+                          <div className="p-4 bg-gray-200 w-full">
 
-                <table className="m-auto w-full text-center table-fixed ">
-                  <tr>
-                    <td className="font-semibold text-right">Employee Id</td>
-                    <td className="font-semibold">:</td>
-                    <td className="text-left break-all">{emp.employeeId}</td>
-                  </tr>
-                  <tr>
-                    <td className="font-semibold text-right">First Name</td>
-                    <td className="font-semibold">:</td>
-                    <td className="text-left break-all">{emp.firstName}</td>
-                  </tr>
-                  <tr>
-                    <td className="font-semibold text-right">Lasr Name</td>
-                    <td className="font-semibold">:</td>
-                    <td className="text-left break-all">{emp.lastName}</td>
-                  </tr>
-                  <tr>
-                    <td className="font-semibold text-right">Email</td>
-                    <td className="font-semibold">:</td>
-                    <td className="text-left break-all">sass</td>
-                  </tr>
-                  <tr>
-                    <td className="font-semibold text-right">Phone</td>
-                    <td className="font-semibold">:</td>
-                    <td className="text-left break-all">{emp.phone}</td>
-                  </tr>
-                  <tr>
-                    <td className="font-semibold text-right">Hire Date</td>
-                    <td className="font-semibold">:</td>
-                    <td className="text-left break-all">hiredddddddjfjfjfjfate</td>
-                  </tr>
-                  <tr>
-                    <td className="font-semibold text-right">Job Id</td>
-                    <td className="font-semibold">:</td>
-                    <td className="text-left break-all">{"dfhsd"}</td>
-                  </tr>
-                  <tr>
-                    <td className="font-semibold text-right">Salary</td>
-                    <td className="font-semibold">:</td>
-                    <td className="text-left break-all">{emp.salary}</td>
-                  </tr>
-                  <tr>
-                    <td className="font-semibold text-right">Manager Id</td>
-                    <td className="font-semibold">:</td>
-                    <td className="text-left break-all">{"mange"}</td>
-                  </tr>
-                  <tr>
-                    <td className="font-semibold text-right">Department Id</td>
-                    <td className="font-semibold">:</td>
-                    <td className="text-left break-all">{emp.departmentId}</td>
-                  </tr>
-                </table>
+                            <table className="m-auto w-full text-center table-fixed ">
+                              <tr>
+                                <td className="font-semibold text-right">Employee Id</td>
+                                <td className="font-semibold">:</td>
+                                <td className="text-left break-all">{emp.id}</td>
+                              </tr>
+                              <tr>
+                                <td className="font-semibold text-right">First Name</td>
+                                <td className="font-semibold">:</td>
+                                <td className="text-left break-all">{emp.fname}</td>
+                              </tr>
+                              <tr>
+                                <td className="font-semibold text-right">Lasr Name</td>
+                                <td className="font-semibold">:</td>
+                                <td className="text-left break-all">{emp.lname}</td>
+                              </tr>
+                              <tr>
+                                <td className="font-semibold text-right">Email</td>
+                                <td className="font-semibold">:</td>
+                                <td className="text-left break-all">{emp.email}</td>
+                              </tr>
+                              <tr>
+                                <td className="font-semibold text-right">Phone</td>
+                                <td className="font-semibold">:</td>
+                                <td className="text-left break-all">{emp.phone}</td>
+                              </tr>
+                              <tr>
+                                <td className="font-semibold text-right">Hire Date</td>
+                                <td className="font-semibold">:</td>
+                                <td className="text-left break-all">{emp.hireDate}</td>
+                              </tr>
+                              <tr>
+                                <td className="font-semibold text-right">Job Id</td>
+                                <td className="font-semibold">:</td>
+                                <td className="text-left break-all">{emp.jobId}</td>
+                              </tr>
+                              <tr>
+                                <td className="font-semibold text-right">Salary</td>
+                                <td className="font-semibold">:</td>
+                                <td className="text-left break-all">{emp.salary}</td>
+                              </tr>
+                              <tr>
+                                <td className="font-semibold text-right">Manager Id</td>
+                                <td className="font-semibold">:</td>
+                                <td className="text-left break-all">{emp.managerId}</td>
+                              </tr>
+                              <tr>
+                                <td className="font-semibold text-right">Department Id</td>
+                                <td className="font-semibold">:</td>
+                                <td className="text-left break-all">{emp.deptId}</td>
+                              </tr>
+                            </table>
 
-              </div>
+                          </div>
 
-              <div className="flex justify-between p-2 font-semibold rounded-b-lg space-x-2 bg-slate-300 border border-t-slate-300">
-                <button className="text-green-700 hover:underline">Update</button>
-                <button className="text-rose-700 hover:underline">Delete</button>
-              </div>
-            </div>
-          )}
-        </Popup>
+                          <div className="flex justify-between p-2 font-semibold rounded-b-lg space-x-2 bg-slate-300 border border-t-slate-300">
+                            <button className="text-green-700 hover:underline">Update</button>
+                            <button className="text-rose-700 hover:underline">Delete</button>
+                          </div>
+                        </div>
+                      )}
+                    </Popup>
                   </td>
                 </tr>
 
@@ -161,7 +166,7 @@ const Employees = () => {
 
           </tbody>
         </table>
-       
+
       </div>
 
     </div>

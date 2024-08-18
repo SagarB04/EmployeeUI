0689fce1cf5351ad88eedf817715cf834c8bea55
Sagar Barman <barmansagar01@gmail.com>
2024-08-18 21:23:05 +0530

    minor changes on department

--- src/components/Departments.jsx
+++ src/components/Departments.jsx
@@ -1,30 +1,9 @@
-import { useEffect, useState } from "react"
 
 const Departments = () => {
 
-  const [num, setNum] = useState(0);
-  const [msg, setMsg] = useState("");
-  const handleChange = (e) => {
-    setNum(Number.parseInt(e.target.value) );
-  }
-  useEffect(() => {
-    
-    setMsg("");
-    if(num<0){
-      setMsg("");
-      setMsg("enter positive number")
-    } else {
-      setMsg("");
-      setMsg(`${num+2} ${num+4} ${num+6}`)
-    }
-
-
-  }, [num])
-
   return (
     <>
-      <input type="number" onChange={(e) => { handleChange(e) }} />
-      <p>{msg}</p>
+      this is department
     </>
   )
 }
